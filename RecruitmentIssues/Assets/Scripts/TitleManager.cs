3f8c593ccf620cefeaefcb1b77517e145cf27498
mon1210 <himejijin@i.softbank.jp>
2024-05-31 21:33:35 +0900

    add Title管理Script名をTitleManagerに変更

--- RecruitmentIssues/Assets/Scripts/TitleManager.cs
+++ RecruitmentIssues/Assets/Scripts/TitleManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class TitleText : MonoBehaviour
+public class TitleManager : MonoBehaviour
 {
     void Update()
     {
