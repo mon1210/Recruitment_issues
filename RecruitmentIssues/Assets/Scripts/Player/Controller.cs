114d332ceaa29e45998185588d6052e487777599
mon1210 <himejijin@i.softbank.jp>
2024-05-31 21:27:48 +0900

    add InputSystemを利用してPlayerが移動できるように

--- RecruitmentIssues/Assets/Scripts/Player/Controller.cs
+++ RecruitmentIssues/Assets/Scripts/Player/Controller.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Windows;
 
 public class Controller : MonoBehaviour
 {
+    // キー入力を受け取って保存する用
+    Vector2 input = Vector2.zero;
+
+    // 
+    [SerializeField] private int MOVE_SPEED = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,22 @@ public class Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        move();
+    }
+
+    // 移動関数
+    private void move()
+    {
+        if(input.x != 0 || input.y != 0)
+        {
+            // 入力された方向に移動
+            transform.Translate(input * MOVE_SPEED * Time.deltaTime);
+        }
+    }
+
+    // キー入力の値を受け取る
+    public void OnMoveEvent(InputAction.CallbackContext context)
+    {
+        input = context.ReadValue<Vector2>();
     }
 }
