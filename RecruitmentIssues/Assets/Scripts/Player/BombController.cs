bdb058dd33da4e8e31bfe467b894afd3ae78a9e7
mon1210 <himejijin@i.softbank.jp>
2024-06-01 23:40:23 +0900

    add 点滅する処理を追加

--- RecruitmentIssues/Assets/Scripts/Player/BombController.cs
+++ RecruitmentIssues/Assets/Scripts/Player/BombController.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class BombController : MonoBehaviour
 {
+    // 点滅周期
+    [SerializeField] private float blinkInterval = 0.0f;
+
+    private SpriteRenderer spriteRenderer;
+    private float timer = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        blinking();
+    }
+
+    // 点滅関数
+    private void blinking()
+    {
+        // 内部時刻を経過させる
+        timer += Time.deltaTime;
+
+        // timer～blinkIntervalの間で繰り返し値を取得
+        float repeatValue = Mathf.Repeat((float)timer, blinkInterval);
+
+        // インターバルの半分以上の時は表示
+        if(repeatValue >= blinkInterval * 0.5f)
+        {
+            spriteRenderer.enabled = true;
+        }
+        else
+        {
+            spriteRenderer.enabled = false;
+        }
+
     }
 }
