cafab891a1cfc4c099911536d6a7c1e1c5646a4f
mon1210 <himejijin@i.softbank.jp>
2024-06-01 17:45:00 +0900

    add PlayerBullet移動関数追加

--- RecruitmentIssues/Assets/Scripts/Player/BulletController.cs
+++ RecruitmentIssues/Assets/Scripts/Player/BulletController.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class BulletController : MonoBehaviour
 {
+    // ˆÚ“®‘¬“x
+    [SerializeField] private int MOVE_SPEED = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,12 @@ public class BulletController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        move();
     }
+
+    private void move()
+    {
+        transform.Translate(0.0f, MOVE_SPEED * Time.deltaTime, 0.0f);
+    }
+
 }
