e13bf64c662b1f939314ae712c4f72ae43457e89
mon1210 <himejijin@i.softbank.jp>
2024-06-02 16:57:08 +0900

    add ランダムな方向に移動するように処理を追加

--- RecruitmentIssues/Assets/Scripts/Bullet/Enemy/RandomEnemyBullet.cs
+++ RecruitmentIssues/Assets/Scripts/Bullet/Enemy/RandomEnemyBullet.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class RandomEnemyBullet : BulletBase
 {
-    // Start is called before the first frame update
+    // 方向
+    private Vector3 direction = Vector3.zero;
+    // 移動速度
+    [SerializeField] private float moveSpeed = 0.0f;
+
     void Start()
-    {
-        
+    {     
+        // ランダムな角度を取得
+        float angle = Random.Range(0f, 360f);
+        // 弧度法へ変換
+        float radians = angle * Mathf.Deg2Rad;
+
+        // 方向ベクトルを設定
+        direction = new Vector3(Mathf.Cos(radians), Mathf.Sin(radians), 0);
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // ランダムな方向に移動
+        transform.Translate(direction * moveSpeed * Time.deltaTime);
     }
 }
