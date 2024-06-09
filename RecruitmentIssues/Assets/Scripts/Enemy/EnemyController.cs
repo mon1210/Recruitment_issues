a3fd2f7d3ded21312dc404bf05836b869af54a64
mon1210 <himejijin@i.softbank.jp>
2024-06-09 19:46:48 +0900

    add 体力減少時赤点滅するように

--- RecruitmentIssues/Assets/Scripts/Enemy/EnemyController.cs
+++ RecruitmentIssues/Assets/Scripts/Enemy/EnemyController.cs
@@ -7,14 +7,21 @@ public class EnemyController : MonoBehaviour
 {
     private EnemyCollider enemyCollider;
     private EnemyBulletManager enemyBulletManager;
+    private SpriteRenderer spriteRenderer;
+
     // 移動速度
     [SerializeField] private float moveSpeed = 0.0f;
     [SerializeField] private GameObject randomBulletPrefab;
     [SerializeField] private GameObject aimedBulletPrefab;
     // 残り体力
     [SerializeField] private int hitPoint = 100;
+    // 点滅周期
+    [SerializeField] private float blinkInterval = 0.0f;
 
     private float moveTimer = 0.0f;
+    private float blinkTimer = 0.0f;
+    private float timer = 0.0f;
+    private bool isBlink = false;
 
     // 移動判定までのタイム
     const float REVERSE_MOVE_TIME = 3.0f;
@@ -24,12 +31,15 @@ public class EnemyController : MonoBehaviour
     const float AIMED_FIRE_INTERVAL = 2.0f;
     // 弾発射位置調整用定数
     const float BULLET_OFFSET_X = 1.0f;
+    // 点滅時間
+    const float BLINK_TIME = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         enemyCollider = GetComponent<EnemyCollider>();
         enemyBulletManager = GetComponent<EnemyBulletManager>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
 
         // ランダムな弾の生成を開始
         StartCoroutine(SpawnRandomBullet());
@@ -44,6 +54,7 @@ public class EnemyController : MonoBehaviour
         if(enemyCollider.IsDamage)
         {
             hitPoint--;
+            isBlink = true;
             enemyCollider.IsDamage = false;
         }
 
@@ -61,6 +72,7 @@ public class EnemyController : MonoBehaviour
             SceneManager.LoadScene("GameOverScene");
         }
 
+        manageBlinking();
     }
 
     // 移動関数
@@ -77,6 +89,45 @@ public class EnemyController : MonoBehaviour
         }
 
         transform.Translate(-moveSpeed * Time.deltaTime, 0.0f, 0.0f);
+    }
+
+    // 点滅処理
+    private void blinking()
+    {
+        // 内部時刻を経過させる
+        timer += Time.deltaTime;
+
+        // timer～blinkIntervalの間で繰り返し値を取得
+        float repeatValue = Mathf.Repeat((float)timer, blinkInterval);
+
+        // インターバルの半分以上の時は表示
+        if (repeatValue >= blinkInterval * 0.5f)
+        {
+            spriteRenderer.color = Color.white;
+        }
+        else
+        {
+            spriteRenderer.color = Color.red;
+        }
+    }
+
+    // 点滅が指定した秒間続くようにする
+    private void manageBlinking()
+    {
+        if (isBlink)
+        {
+            blinkTimer -= Time.deltaTime;
+            if (blinkTimer > 0)
+            {
+                blinking();
+            }
+            else
+            {
+                // リセット
+                blinkTimer = BLINK_TIME;
+                isBlink = false;
+            }
+        }
     }
 
     // ランダムな弾を生成する
