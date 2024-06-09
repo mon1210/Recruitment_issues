d562255e8835e4ebd0d199b5a884d47f01d06d23
mon1210 <himejijin@i.softbank.jp>
2024-06-09 18:04:14 +0900

    add 弾がPlayerに向かって飛んでいくように

--- RecruitmentIssues/Assets/Scripts/Bullet/Enemy/AimedEnemyBullet.cs
+++ RecruitmentIssues/Assets/Scripts/Bullet/Enemy/AimedEnemyBullet.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class AimedEnemyBullet : MonoBehaviour
 {
+    private GameObject player;
+    private GameObject enemy;
+
     void Start()
     {
-        
+        player = GameObject.Find("Player");
+
+        enemy = GameObject.Find("Enemy");
+
+        Vector2 vec = player.transform.position - enemy.transform.position;
+        GetComponent<Rigidbody2D>().velocity = vec;
+
     }
 
     void Update()
     {
-        
+        move();
+    }
+
+    private void move()
+    {
+
     }
 }
