1a970c4767d4d500def8e42d3652248f20f1e7bd
mon1210 <himejijin@i.softbank.jp>
2024-06-13 11:02:18 +0900

    add 弾が進む方向を向くように

--- RecruitmentIssues/Assets/Scripts/Bullet/Enemy/AimedEnemyBullet.cs
+++ RecruitmentIssues/Assets/Scripts/Bullet/Enemy/AimedEnemyBullet.cs
@@ -15,6 +15,9 @@ public class AimedEnemyBullet : MonoBehaviour
 
         // 自身の生成位置(Enemy)からPlayerまでのベクトルを取り、向かう
         Vector2 vec = player.transform.position - enemy.transform.position;
+        // 進む位置を向くように
+        transform.rotation = Quaternion.FromToRotation(Vector3.up, vec);
+        // 移動
         GetComponent<Rigidbody2D>().velocity = vec;
 
     }
